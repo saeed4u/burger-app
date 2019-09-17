0a5e617f22df3e0f39c581f085d0ccac260ad0dc
= <saeedissah@gmail.com>
2019-09-17 22:50:55 +0300

    Added dynamic content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout'
+import BurgerBuild from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Layout>
+          <BurgerBuild></BurgerBuild>
+        </Layout>
       </div>
     );
   }
