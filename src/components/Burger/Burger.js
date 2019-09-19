c3f4ff37b8a65e70a274aa0eeb26e5cace8f8121
= <=>
2019-09-19 21:10:18 +0300

    Updated burger builder

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import PropTyps from 'prop-types';
+
+import classes from './Burger.css'
+
+const Burger = (props) => {
+
+    console.log(props.ingredients);
+
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_,index) => <BurgerIngredient key={igKey + index} type={igKey} />)
+        })
+        .reduce((prev, current) => {
+            return prev.concat(current);
+        },[])
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please add an ingredient</p>
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top'/>
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom'/>
+        </div>
+    )
+}
+
+Burger.propTypes = {
+    ingredients: PropTyps.array.isRequired
+};
+
+export default Burger;
